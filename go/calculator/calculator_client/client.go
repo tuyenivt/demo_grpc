3ea59199173c6db639489ea1afefe4a2cc1c0374
Zero <tuyenivt@gmail.com>
2018-09-09 21:47:43 +0700

    implement server streaming calculator  (Prime Number Decomposition)

--- go/calculator/calculator_client/client.go
+++ go/calculator/calculator_client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 
 	"../calculatorpb"
@@ -10,15 +11,17 @@ import (
 )
 
 func main() {
-	fmt.Printf("[calculator] Hello! This is client")
+	fmt.Println("[calculator] Hello! This is client")
 	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
 	if err != nil {
-		log.Fatalf("[calculator] Could not connect: %v", err)
+		log.Fatalf("[calculator] Could not connect: %v\n", err)
 	}
 	defer cc.Close()
 	c := calculatorpb.NewCalculatorServiceClient(cc)
 
-	doUnary(c)
+	// doUnary(c)
+
+	doServerStreaming(c)
 }
 
 func doUnary(c calculatorpb.CalculatorServiceClient) {
@@ -29,7 +32,30 @@ func doUnary(c calculatorpb.CalculatorServiceClient) {
 	}
 	res, err := c.Sum(context.Background(), req)
 	if err != nil {
-		log.Fatalf("[calculator] error while calling Sum RPC: %v", err)
+		log.Fatalf("[calculator] error while calling Sum RPC: %v\n", err)
 	}
-	log.Printf("[calculator] Response from Sum: %v", res.SumResult)
+	log.Printf("[calculator] Response from Sum: %v\n", res.SumResult)
+}
+
+func doServerStreaming(c calculatorpb.CalculatorServiceClient) {
+	fmt.Println("[calculator] Starting to do a PrimeNumberDecomposition Server Streaming RPC...")
+	req := &calculatorpb.PrimeNumberDecompositionRequest{
+		Number: 120,
+	}
+	resStream, err := c.PrimeNumberDecomposition(context.Background(), req)
+	if err != nil {
+		log.Fatalf("[calculator] error while calling PrimeNumberDecomposition RPC: %v\n", err)
+	}
+	for {
+		res, err := resStream.Recv()
+		if err == io.EOF {
+			// we're reached the end of the stream
+			break
+		}
+		if err != nil {
+			log.Fatalf("[calculator] error while reading stream: %v\n", err)
+		}
+		fmt.Println(res.GetPrimeFactor())
+	}
+
 }
--- go/calculator/calculator_server/server.go
+++ go/calculator/calculator_server/server.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 
 	"../calculatorpb"
 	"google.golang.org/grpc"
@@ -23,15 +24,34 @@ func (*server) Sum(ctx context.Context, req *calculatorpb.SumRequest) (*calculat
 	return res, nil
 }
 
+func (*server) PrimeNumberDecomposition(req *calculatorpb.PrimeNumberDecompositionRequest, stream calculatorpb.CalculatorService_PrimeNumberDecompositionServer) error {
+	fmt.Printf("PrimeNumberDecomposition function was invoked with %v\n", req)
+	number := req.GetNumber()
+	divisor := int64(2)
+	for number > 1 {
+		if number%divisor == 0 {
+			stream.Send(&calculatorpb.PrimeNumberDecompositionResponse{
+				PrimeFactor: divisor,
+			})
+			number = number / divisor
+		} else {
+			divisor++
+			fmt.Printf("Divisor has increased to %v\n", divisor)
+		}
+		time.Sleep(1000 * time.Millisecond)
+	}
+	return nil
+}
+
 func main() {
-	fmt.Printf("[calculator] Hello! This is server")
+	fmt.Println("[calculator] Hello! This is server")
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 	if err != nil {
-		log.Fatalf("[calculator] Failed to listen: %v", err)
+		log.Fatalf("[calculator] Failed to listen: %v\n", err)
 	}
 	s := grpc.NewServer()
 	calculatorpb.RegisterCalculatorServiceServer(s, &server{})
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("[calculator] Failed to serve: %v", err)
+		log.Fatalf("[calculator] Failed to serve: %v\n", err)
 	}
 }
