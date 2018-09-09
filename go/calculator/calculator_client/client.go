1eb8371e30b601a3a751800ec037f531dd1b1ae1
Zero <tuyenivt@gmail.com>
2018-09-09 23:48:54 +0700

    implement client streaming calculator  (ComputeAverage)

--- go/calculator/calculator_client/client.go
+++ go/calculator/calculator_client/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"time"
 
 	"../calculatorpb"
 	"google.golang.org/grpc"
@@ -21,7 +22,9 @@ func main() {
 
 	// doUnary(c)
 
-	doServerStreaming(c)
+	// doServerStreaming(c)
+
+	doClientStreaming(c)
 }
 
 func doUnary(c calculatorpb.CalculatorServiceClient) {
@@ -57,5 +60,26 @@ func doServerStreaming(c calculatorpb.CalculatorServiceClient) {
 		}
 		fmt.Println(res.GetPrimeFactor())
 	}
+}
 
+func doClientStreaming(c calculatorpb.CalculatorServiceClient) {
+	fmt.Println("[calculator] Starting to do a ComputeAverage Server Streaming RPC...")
+	stream, err := c.ComputeAverage(context.Background())
+	if err != nil {
+		log.Fatalf("[calculator] error while calling ComputeAverage RPC: %v\n", err)
+	}
+	numbers := []float64{1.0, 2.0, 3.0, 4.0}
+	// we iterate over our slice and send each message individually
+	for _, number := range numbers {
+		fmt.Printf("[calculator] Sending number: %v\n", number)
+		stream.Send(&calculatorpb.ComputeAverageRequest{
+			Number: number,
+		})
+		time.Sleep(100 * time.Millisecond)
+	}
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("[calculator] error while receiving response from ComputeAverage: %v", err)
+	}
+	fmt.Printf("[calculator] Response from ComputeAverage: %v\n", res.GetAverage())
 }
--- go/calculator/calculator_server/server.go
+++ go/calculator/calculator_server/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"time"
@@ -14,7 +15,7 @@ import (
 type server struct{}
 
 func (*server) Sum(ctx context.Context, req *calculatorpb.SumRequest) (*calculatorpb.SumResponse, error) {
-	fmt.Printf("Sum function was invoked with %v", req)
+	fmt.Printf("[calculator] Sum function was invoked with %v", req)
 	first := req.GetFirstNumber()
 	second := req.GetSecondNumber()
 	result := first + second
@@ -25,7 +26,7 @@ func (*server) Sum(ctx context.Context, req *calculatorpb.SumRequest) (*calculat
 }
 
 func (*server) PrimeNumberDecomposition(req *calculatorpb.PrimeNumberDecompositionRequest, stream calculatorpb.CalculatorService_PrimeNumberDecompositionServer) error {
-	fmt.Printf("PrimeNumberDecomposition function was invoked with %v\n", req)
+	fmt.Printf("[calculator] PrimeNumberDecomposition function was invoked with %v\n", req)
 	number := req.GetNumber()
 	divisor := int64(2)
 	for number > 1 {
@@ -36,11 +37,32 @@ func (*server) PrimeNumberDecomposition(req *calculatorpb.PrimeNumberDecompositi
 			number = number / divisor
 		} else {
 			divisor++
-			fmt.Printf("Divisor has increased to %v\n", divisor)
+			fmt.Printf("[calculator] Divisor has increased to %v\n", divisor)
 		}
 		time.Sleep(1000 * time.Millisecond)
 	}
 	return nil
+}
+
+func (*server) ComputeAverage(stream calculatorpb.CalculatorService_ComputeAverageServer) error {
+	fmt.Println("[calculator] ComputeAverage function was invoked with a streaming request")
+	sum := float64(0)
+	count := 0
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			// we have finished reading the client stream
+			return stream.SendAndClose(&calculatorpb.ComputeAverageResponse{
+				Average: sum / float64(count),
+			})
+		}
+		if err != nil {
+			log.Fatalf("[calculator] error while reading client stream: %v", err)
+		}
+		sum += req.GetNumber()
+		count++
+	}
+
 }
 
 func main() {
