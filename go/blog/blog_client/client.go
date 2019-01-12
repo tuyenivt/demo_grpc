92085ccacbf70ce171585d89a831f11b93d9214e
Tuyen <tuyenivt@gmail.com>
2019-01-12 17:42:44 +0700

    ListBlog Client

--- go/blog/blog_client/client.go
+++ go/blog/blog_client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 
 	"../blogpb"
@@ -72,4 +73,20 @@ func main() {
 		fmt.Printf("Error happened while deleting: %v \n", updateErr)
 	}
 	fmt.Printf("Blog was deleted: %v \n", deleteRes)
+
+	// list Blogs
+	stream, err := c.ListBlog(context.Background(), &blogpb.ListBlogRequest{})
+	if err != nil {
+		log.Fatalf("error while calling ListBlog RPC: %v", err)
+	}
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Something happened: %v", err)
+		}
+		fmt.Println(res.GetBlog())
+	}
 }
