b90040717d45e7683337ada5cde764238990e16c
Tuyen <tuyenivt@gmail.com>
2019-01-12 16:53:13 +0700

    CreateBlog Client

---
+++ go/blog/blog_client/client.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"../blogpb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println("Blog Client")
+
+	opts := grpc.WithInsecure()
+
+	cc, err := grpc.Dial("localhost:51051", opts)
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+	}
+	defer cc.Close()
+
+	c := blogpb.NewBlogServiceClient(cc)
+
+	// create Blog
+	fmt.Println("Creating the blog")
+	blog := &blogpb.Blog{
+		AuthorId: "Tuyen",
+		Title:    "My First Blog",
+		Content:  "Content of the first blog",
+	}
+	createBlogRes, err := c.CreateBlog(context.Background(), &blogpb.CreateBlogRequest{Blog: blog})
+	if err != nil {
+		log.Fatalf("Unexpected error: %v", err)
+	}
+	fmt.Printf("Blog has been created: %v", createBlogRes)
+}
